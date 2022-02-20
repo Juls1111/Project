64189ce0217edf99c8e86350da2f3d8311195c9d
Juls1111 <99805587+Juls1111@users.noreply.github.com>
2022-02-20 21:26:08 +0300

    Add files via upload
    task 2

---
+++ main2.py
@@ -0,0 +1,6 @@
+firstname = input('Insert your first name here.')
+lastname = input('Insert your last name here.')
+firstname = firstname.upper()
+lastname = lastname.upper()
+data = firstname + " " + lastname
+print(data)
